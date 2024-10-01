5a121cc362bc1e830494b490e25eadef0de97a4b
Prince-Mercy Habtemariam <finalshow64@gmail.com>
2024-10-01 17:42:02 +0200

    Exercise 3.3

--- src/graph-plotter/Sampler.h
+++ src/graph-plotter/Sampler.h
@@ -2,19 +2,18 @@
 #define SAMPLER_H
 
 #include "DataPoints.h"
-#include "Sinusoid.h"
+#include <functional>
 // Logic Layer: Generates samples of mathematical functions for plotting.
 class Sampler
 {
 public:
 	// performs uniform sampling
-	DataPoints generateSamples(const Sinusoid &sine_wave, const Range &range) const;
+	DataPoints generateSamples(std::function<float(float)> func, const Range& range) const;
 
 private:
 	static const int TOTAL_POINTS = 200; // increase the number of data points generated, hence increase sample size by 4x inital size
 };
 
 // standalone function for generating data points, a default uniform sampler is provided
-DataPoints generateDataPoints(const Sinusoid &sine_wave, const Range &range, const Sampler &sampler = Sampler{});
-
+DataPoints generateDataPoints(std::function<float(float)> func, const Range& range, const Sampler& sampler = Sampler{});
 #endif
--- src/graph-plotter/main.cpp
+++ src/graph-plotter/main.cpp
@@ -7,46 +7,39 @@
 #include "DottedLineStyle.h"
 #include "Points.h"
 #include <memory>
+#include <cmath>
 
 using std::make_shared;
 using std::shared_ptr;
 
 int main()
 {
-	// setup Graph with Display
-	const auto WIDTH = 800;
-	const auto HEIGHT = 600;
-	auto display = make_shared<Display>(WIDTH, HEIGHT);
-	auto graph = Graph{display};
+    // setup Graph with Display
+    const auto WIDTH = 800;
+    const auto HEIGHT = 600;
+    auto display = make_shared<Display>(WIDTH, HEIGHT);
+    auto graph = Graph{ display };
 
-	// create sine and cosine functions
-	auto amplitude = 1.0f;
-	auto frequency = 1.0f;
-	auto phase = 0.0f;
-	auto sine_function = Sinusoid{amplitude, frequency, phase};
-	auto cosine_function = Sinusoid{amplitude, frequency, phase + PI / 2};
+    // Create a range for the x-values
+    auto range = Range{ -3.0f, 1.5f };
 
-	// generate range and plot graphs
-	auto range = Range{0, 6 * PI};
-	auto solid_red = SolidLineStyle{Colour::Red, display};
-	graph.plot(generateDataPoints(sine_function, range), solid_red);
+    // Polynomial function: y = x^2 + 2x + 1
+    auto polynomial = [](float x) {
+        return x * x + 2 * x + 1;
+        };
 
-	auto solid_blue = SolidLineStyle{Colour::Blue, display};
-	graph.plot(generateDataPoints(cosine_function, range), solid_blue);
+    // Exponential function: y = e^(1.5x)
+    auto exponential = [](float x) {
+        return std::exp(1.5f * x);
+        };
 
-	//new plots for 3.2
+    // Plot polynomial with solid red line style
+    auto solid_red = SolidLineStyle{ Colour::Red, display };
+    graph.plot(generateDataPoints(polynomial, range), solid_red);
 
-	// Dashed line plot
-    auto dashed_green = DashedLineStyle{Colour::Green, display};
-    graph.plot(generateDataPoints(cosine_function, range), dashed_green);
+    // Plot exponential with solid blue line style
+    auto solid_blue = SolidLineStyle{ Colour::Blue, display };
+    graph.plot(generateDataPoints(exponential, range), solid_blue);
 
-    // Dotted line plot
-    auto dotted_blue = DottedLineStyle{Colour::Blue, display};
-    graph.plot(generateDataPoints(sine_function, range), dotted_blue);
-
-	//The sinusoidal graphs are not smooth because the plotter is generating insufficient data points over the graph's x-range.
-	//Due to this, there are too few points hence the curves look jagged instead of smooth.
-	//To fix this, the number of samples for each period needs to be increased.
-
-	return 0;
+    return 0;
 }
